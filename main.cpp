1c673638784581aa6d7ee151d0378cf40ce29d0a
boocmp <boocmp@yandex.ru>
2022-11-09 16:16:14 +0700

    Using atlas.

--- main.cpp
+++ main.cpp
@@ -9,14 +9,40 @@ class Ferma : public app::GameApp {
   void Initialize() override {
     render::LoadResource("resources/images/egg.png", "egg"); 
     render::LoadResource("resources/images/backdrop.png", "backdrop");
-    render::LoadResource("resources/images/duck.png", "duck");
+
+    auto duck_atlas = render::Atlas::Create("resources/images/duck.png", "duck");
+    duck_atlas.AddAnimationLine("down").SetFramesCount(9, true).SetFrameHeight(83).SetFrameWidth(56);
+    duck_atlas.AddAnimationLine("down_left").SetFramesCount(9, true).SetFrameHeight(83).SetFrameWidth(56);
+    duck_atlas.AddAnimationLine("left").SetFramesCount(9, true).SetFrameHeight(83).SetFrameWidth(56);
+    duck_atlas.AddAnimationLine("down_right").SetFramesCount(9, true).SetFrameHeight(83).SetFrameWidth(56);
+    duck_atlas.AddAnimationLine("right").SetFramesCount(9, true).SetFrameHeight(83).SetFrameWidth(56);
+    duck_atlas.AddAnimationLine("up").SetFramesCount(9, true).SetFrameHeight(83).SetFrameWidth(56);
+    duck_atlas.AddAnimationLine("up_right").SetFramesCount(9, true).SetFrameHeight(83).SetFrameWidth(56);
+    duck_atlas.AddAnimationLine("up_left").SetFramesCount(9, true).SetFrameHeight(83).SetFrameWidth(56);
+    render::BakeAtlas(duck_atlas);
   }
 
   void Render() override {
     render::DrawImage("backdrop", 1, 1, 800, 600);
 
-    int image = (left*down) + 2*(left*!up*!down) + 3*(right*down) + 4*(right*!up*!down) + 5*(up*!right*!left) + 6*(up*right) + 7*(up*left); 
-    render::DrawImageFromAtlas("duck", x, y, 56, 83, ((frame % 27)/3)*56, image*83, 56, 83);
+    //int image = (left*down) + 2*(left*!up*!down) + 3*(right*down) + 4*(right*!up*!down) + 5*(up*!right*!left) + 6*(up*right) + 7*(up*left); 
+    //render::DrawImageFromAtlas("duck", x, y, 56, 83, ((frame % 27)/3)*56, image*83, 56, 83);
+
+    const std::string animation = [&]() {
+      if (up && left) return "up_left";
+      if (up && right) return "up_right";
+      if (down && left) return "down_left";
+      if (down && right) return "down_right";
+
+      if (up) return "up";
+      if (down) return "down";
+      if (left) return "left";
+      if (right) return "right";
+
+      return "down";
+    }();
+
+    render::DrawImageFromAtlas("duck", animation, frame, x, y);
     
     right = 0;
     left = 0;
